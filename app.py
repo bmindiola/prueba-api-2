74749a1a73c2f9aac8685b43037ecd8d3b42b18b
Mindiola <maikmj101@gmail.com>
2022-11-05 09:48:36 -0500

    Backend Resultados Python-Flask Completo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, Response, render_template
+from flask import Flask, jsonify, request, Response
 import pymongo
 import certifi
 from bson import json_util
@@ -23,8 +23,9 @@ def home():
     return jsonify({"mensaje": "APP Votación"})
     #return render_template('index.html')
 
-# Metods: POST
 
+# ------------- MESAS ----------------------
+# Metods: POST
 # Opcion para agregar mesas a la base de datos.
 @app.route('/mesas', methods=['POST'])
 def addMesa():
@@ -41,6 +42,45 @@ def addMesa():
         return response
     return notFound()
 
+# Metods: GET
+# Opcion para obtener los datos todas las mesas registradas.
+@app.route('/mesas', methods=['GET'])
+def getMesas():
+    mesas = baseDatos.mesas.find()
+    response = json_util.dumps(mesas)
+    return Response (response, mimetype='application/json')
+
+# Opcion para obtener los datos de una mesa registrada por su id.
+@app.route('/mesas/<id>', methods=['GET'])
+def getMesa(id):
+    mesa = baseDatos.mesas.find_one({"_id": ObjectId(id)})
+    response = json_util.dumps(mesa)
+    return Response (response, mimetype='application/json')
+
+# Metods: PUT
+# Opcion para actualizar los datos de una mesa por su id.
+@app.route('/mesas/update/<id>', methods=['PUT'])
+def updateMesa(id):
+    mesa = request.json['mesa']
+    cedulas_inscritas = request.json['cedulas_inscritas']
+
+    if mesa and cedulas_inscritas:
+        baseDatos.mesas.update_one({"_id": ObjectId(id)}, {'$set' : 
+                                    {'mesa' : mesa, 'cedulas_inscritas' : cedulas_inscritas}})
+        response = jsonify({'message' : 'Mesa ' + id + ' actualizada correctamente'})
+        return response
+    return notFound()
+
+# Metods: DELETE
+#Opción para eliminar una mesa por su id.
+@app.route('/mesas/delete/<id>', methods=['DELETE'])
+def deleteMesa(id):
+    baseDatos.mesas.delete_one({"_id": ObjectId(id)})
+    return jsonify({'message' : 'Mesa '+ id +' borrada correctamente'})
+
+
+# ------------- PARTIDOS ----------------------
+# Metods: POST
 # Opcion para agregar partidos a la base de datos.
 @app.route('/partidos', methods=['POST'])
 def addPartido():
@@ -57,6 +97,45 @@ def addPartido():
         return response
     return notFound()
 
+# Metods: GET
+# Opcion para obtener los datos todos los partidos registrados.
+@app.route('/partidos', methods=['GET'])
+def getPartidos():
+    partidos = baseDatos.partidos.find()
+    response = json_util.dumps(partidos)
+    return Response (response, mimetype='application/json')
+
+# Opcion para obtener los datos de un partido registrado por su id.
+@app.route('/partidos/<id>', methods=['GET'])
+def getPartido(id):
+    partido = baseDatos.partidos.find_one({"_id": ObjectId(id)})
+    response = json_util.dumps(partido)
+    return Response (response, mimetype='application/json')
+
+# Metods: PUT
+# Opcion para actualizar los datos de un partido por su id.
+@app.route('/partidos/update/<id>', methods=['PUT'])
+def updatePartido(id):
+    nombrePartido = request.json['nombrePartido']
+    lema = request.json['lema']
+
+    if nombrePartido and lema:
+        baseDatos.partidos.update_one({"_id": ObjectId(id)}, {'$set' : 
+                                    {'nombrePartido' : nombrePartido, 'lema' : lema}})
+        response = jsonify({'message' : 'Partido ' + id + ' actualizado correctamente'})
+        return response
+    return notFound()
+
+# Metods: DELETE
+#Opción para eliminar un partido por su id.
+@app.route('/partidos/delete/<id>', methods=['DELETE'])
+def deletePart(id):
+    baseDatos.partidos.delete_one({"_id": ObjectId(id)})
+    return jsonify({'message' : 'Partido '+ id +' borrado correctamente'})
+
+
+# ------------- CANDIDATOS ----------------------
+# Metods: POST
 # Opcion para agregar candidatos a la base de datos.
 @app.route('/candidatos', methods=['POST'])
 def addCandidato():
@@ -79,6 +158,49 @@ def addCandidato():
         return response
     return notFound()
 
+# Metods: GET
+# Opcion para obtener los datos todos los candidatos registrados.
+@app.route('/candidatos', methods=['GET'])
+def getCandidatos():
+    candidatos = baseDatos.candidatos.find()
+    response = json_util.dumps(candidatos)
+    return Response (response, mimetype='application/json')
+
+# Opcion para obtener los datos de un candidato registrado por su id.
+@app.route('/candidatos/<id>', methods=['GET'])
+def getCandidato(id):
+    candidato = baseDatos.candidatos.find_one({"_id": ObjectId(id)})
+    response = json_util.dumps(candidato)
+    return Response (response, mimetype='application/json')
+
+# Metods: PUT
+# Opcion para actualizar los datos de un candidato por su id.
+@app.route('/candidatos/update/<id>', methods=['PUT'])
+def updateCandidato(id):
+    numero = request.json['numero']
+    cedula = request.json['cedula']
+    nombre = request.json['nombre']
+    apellido = request.json['apellido']
+    partido = request.json['partido']
+
+    if numero and cedula and nombre and apellido and partido:
+        baseDatos.candidatos.update_one({"_id": ObjectId(id)}, {'$set' : 
+                                        {'numero' : numero, 'cedula' : cedula, 'nombre' : nombre, 
+                                        'apellido' : apellido, 'partido' : partido}})
+        response = jsonify({'message' : 'Candidato ' + id + ' actualizado correctamente'})
+        return response
+    return notFound()
+
+# Metods: DELETE
+#Opción para eliminar un candidato por su id.
+@app.route('/candidatos/delete/<id>', methods=['DELETE'])
+def deleteCand(id):
+    baseDatos.candidatos.delete_one({"_id": ObjectId(id)})
+    return jsonify({'message' : 'Candidato '+ id +' borrado correctamente'})
+
+
+# ------------- RESULTADOS ----------------------
+# Metods: POST
 # Opcion para agregar resultados a la base de datos.
 @app.route('/resultados', methods=['POST'])
 def addResultado():
@@ -98,45 +220,48 @@ def addResultado():
         })
         return response
     return notFound()
-            
 
 # Metods: GET
-# Opcion para obtener los datos todas las mesas registradas.
-@app.route('/mesas', methods=['GET'])
-def getMesas():
-    mesas = baseDatos.mesas.find()
-    response = json_util.dumps(mesas)
+# Opcion para obtener los datos todos los partidos registradas.
+@app.route('/resultados', methods=['GET'])
+def getResultados():
+    resultados = baseDatos.resultados.find()
+    response = json_util.dumps(resultados)
     return Response (response, mimetype='application/json')
 
-# Opcion para obtener los datos de una mesa registrada por su id.
-@app.route('/mesas/<id>', methods=['GET'])
-def getMesa(id):
-    mesa = baseDatos.mesas.find_one({"_id": ObjectId(id)})
-    response = json_util.dumps(mesa)
+# Opcion para obtener los datos de un partido registrado por su id.
+@app.route('/resultados/<id>', methods=['GET'])
+def getResultado(id):
+    resultado = baseDatos.resultados.find_one({"_id": ObjectId(id)})
+    response = json_util.dumps(resultado)
     return Response (response, mimetype='application/json')
-
 
 # Metods: PUT
-# Opcion para actualizar los datos de una mesa por su id.
-@app.route('/mesas/update/<id>', methods=['PUT'])
-def updateMesa(id):
+# Opcion para actualizar los datos de un resultado por su id.
+@app.route('/resultados/update/<id>', methods=['PUT'])
+def updateResultado(id):
     mesa = request.json['mesa']
-    cedulas_inscritas = request.json['cedulas_inscritas']
+    numeroCandidato = request.json['numeroCandidato']
+    partido = request.json['partido']
+    votos = request.json['votos']
 
-    if mesa and cedulas_inscritas:
-        baseDatos.mesas.update_one({"_id": ObjectId(id)}, {'$set' : {'mesa' : mesa, 'cedulas_inscritas' : cedulas_inscritas}})
-        response = jsonify({'message' : 'Mesa ' + id + ' actualizada correctamente'})
+    if mesa and numeroCandidato and partido and votos:
+        baseDatos.resultados.update_one({"_id": ObjectId(id)}, {'$set' : 
+                                        {'mesa' : mesa, 'numeroCandidato' : numeroCandidato, 'partido' : partido, 
+                                        'votos' : votos}})
+        response = jsonify({'message' : 'Resultado ' + id + ' actualizado correctamente'})
         return response
     return notFound()
 
-
 # Metods: DELETE
-#Opción para eliminar una mesa por su id.
-@app.route('/mesas/delete/<id>', methods=['DELETE'])
-def delete(id):
-    baseDatos.mesas.delete_one({"_id": ObjectId(id)})
-    return jsonify({'message' : 'Mesa '+ id +' borrada correctamente'})
+#Opción para eliminar un resultado por su id.
+@app.route('/resultados/delete/<id>', methods=['DELETE'])
+def deleteRes(id):
+    baseDatos.resultados.delete_one({"_id": ObjectId(id)})
+    return jsonify({'message' : 'Resultado '+ id +' borrado correctamente'})
 
+
+#----------------ERROR-----------------------
 @app.errorhandler(404)
 def notFound(error=None):
     message ={
